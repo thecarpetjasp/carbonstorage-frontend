044249bb3eefd9a5a1da48b7e1b397d0470c7519
thecarpetjasp <thecarpetjasp@gmail.com>
2025-03-15 04:11:19 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    particlesJS.load("particles-js", "assets/js/particles-config.json", function () {
+        console.log("Particles.js loaded successfully!");
+    });
+});
